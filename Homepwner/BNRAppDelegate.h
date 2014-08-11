b23dc8f22cc3917513a4b7d7f8bcbc1648299dc7
Brigitte Michau <brigitte@fontera.com>
2014-08-11 21:09:57 +0200

    Initial Commit

---
+++ Homepwner/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Homepwner
+//
+//  Created by Brigitte Michau on 2014/08/11.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
