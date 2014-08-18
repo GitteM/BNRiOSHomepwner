534ce6466614a0a552fe5747732c162a2061f85c
Brigitte Michau <brigitte@fontera.com>
2014-08-18 10:54:24 +0200

    Chapter 8 - UITableView and UITableViewController. Completed Gold Challenge

--- Homepwner/BNRItemsViewController.m
+++ Homepwner/BNRItemsViewController.m
@@ -34,55 +34,89 @@
     
     [super viewDidLoad];
     
+    UIImageView *backgroundImage = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"bg640x1136.png"]];
+    self.tableView.backgroundView = backgroundImage;
+    
     self.tableView.contentInset = UIEdgeInsetsMake(20.0, 0.0, 0.0, 0.0);
+    self.tableView.rowHeight = 60.0;
+    
+    
     
     [self.tableView registerClass:[UITableViewCell class]
            forCellReuseIdentifier:@"UITableViewCell"];
+
+    
 }
 
 #pragma mark - UITableViewDataSource Protocol Required Methods
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-   
+    
     // Get a new or recycled cell
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
                                                             forIndexPath:indexPath];
+    
+    cell.userInteractionEnabled = NO;
+    cell.backgroundColor = [UIColor clearColor];
     
     // Set the text on the cell with the description of item
     // that is at the nth index of items, where n = row this cell
     // will appear on in the tableview
     
-    if (indexPath.section == 0) {
-        
-        if (indexPath.row == [[[BNRItemStore sharedStore]expensiveItems] count]) {
-            [self lastRowOfTableView:cell];
-        } else {
-            NSArray *items = [[BNRItemStore sharedStore]expensiveItems];
-            BNRItem *item = items[indexPath.row];
-            cell.textLabel.text = [item description];
-        }
-        
-    } else {
-        
-        if (indexPath.row == [[[BNRItemStore sharedStore]economicalItems] count]) {
-            [self lastRowOfTableView:cell];
-        } else {
-            NSArray *items = [[BNRItemStore sharedStore]economicalItems];
-            BNRItem *item = items[indexPath.row];
-            cell.textLabel.text = [item description];
-        }
+    switch (indexPath.section) {
+        case 0: // Expensive Items
+            if (indexPath.row == [[[BNRItemStore sharedStore]expensiveItems] count]) {
+                [self lastRowOfTableView:cell];
+            } else {
+                NSArray *items = [[BNRItemStore sharedStore]expensiveItems];
+                BNRItem *item = items[indexPath.row];
+                cell.textLabel.text = [item description];
+                [self itemsFontForCell:cell];
+            } break;
+        case 1: // Economical Items
+            if (indexPath.row == [[[BNRItemStore sharedStore]economicalItems] count]) {
+                [self lastRowOfTableView:cell];
+            } else {
+                NSArray *items = [[BNRItemStore sharedStore]economicalItems];
+                BNRItem *item = items[indexPath.row];
+                cell.textLabel.text = [item description];
+                [self itemsFontForCell:cell];
+            } break;
+        default:
+            break;
     }
-    
-    UIFont *myFont = [ UIFont fontWithName: @"Arial" size: 14.0 ];
-    cell.textLabel.font  = myFont;
-    cell.textLabel.textColor = [UIColor darkGrayColor];
-    cell.userInteractionEnabled = NO;
     
     return cell;
 }
 
-- (void)lastRowOfTableView:(UITableViewCell *)cell  {
+- (void) itemsFontForCell:(UITableViewCell *)cell {
+    UIFont *myFont = [ UIFont fontWithName: @"Arial" size: 18.0 ]; // asking for 20.0 but it is too big
+    cell.textLabel.font  = myFont;
+    cell.textLabel.textColor = [UIColor darkGrayColor];
+}
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    float rowHeight = 60;
+    
+    switch (indexPath.section) {
+        case 0: // Expensive Items
+            if (indexPath.row == [[[BNRItemStore sharedStore]expensiveItems] count]) {
+                rowHeight = 44.0;
+            } break;
+        case 1: // Economical Items
+            if (indexPath.row == [[[BNRItemStore sharedStore]economicalItems] count]) {
+                rowHeight = 44.0;
+            } break;
+        default: break;
+    }
+    
+    return rowHeight;
+}
+
+- (void)lastRowOfTableView:(UITableViewCell *)cell {
+    
+    UIFont *myFont = [ UIFont fontWithName: @"Arial" size: 14.0 ];
+    cell.textLabel.font  = myFont;
     cell.textLabel.text = @"No More Items";
     cell.textLabel.textColor = [UIColor blackColor];
     cell.textLabel.textAlignment = NSTextAlignmentRight;
