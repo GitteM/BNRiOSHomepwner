e181a875f13dbdc8005452f9bb36f1882a866490
Brigitte Michau <brigitte@fontera.com>
2014-08-26 19:15:20 +0200

    Chapter 10 UINavigationController - An additional view controller

---
+++ Homepwner/BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by Brigitte Michau on 2014/08/26.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
---
+++ Homepwner/BNRDetailViewController.m
@@ -0,0 +1,22 @@
+//
+//  BNRDetailViewController.m
+//  Homepwner
+//
+//  Created by Brigitte Michau on 2014/08/26.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+
+@interface BNRDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
+
+@implementation BNRDetailViewController
+
+@end
